191bee64719927e16e4e94917d4e54ebd0b64c34
Adam Piper <adam.piper@cgi.com>
2010-09-16 20:49:56 +0000

    Add changes to ProjectNotice so that it works now :-) See #296
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@1303 484df14f-75d2-4b3f-8ba0-490e05069a01

--- projectnotice/web_ui.py
+++ projectnotice/web_ui.py
@@ -1,50 +1,78 @@
 from trac.core import *
 from trac.config import *
 from trac.admin.api import IAdminPanelProvider
-from trac.web.chrome import add_stylesheet, add_script, add_notice, add_warning
+from trac.web import ITemplateStreamFilter
+from trac.web.chrome import add_stylesheet, add_script, add_notice, add_warning, ITemplateProvider
 from trac.util.translation import _
 from genshi.filters.transform import Transformer
 from genshi.builder import tag
 
 class ProjectNotice(Component):
 
-    """Plugin to allow us to post notices on projects,
+    """Plugin to allow the posting of notices on projects,
     disable access to a project (for maintenance, etc.)"""
 
-    implements(IAdminPanelProvider, ITemplateStreamFilter)
+    implements(IAdminPanelProvider, ITemplateStreamFilter, ITemplateProvider)
 
     def get_notice(self):
         """Retrieve the notice from the DB or None if not set"""
-        cursor = self.env.get_read_db().cursor
-        (text,) = cursor.execute("SELECT value FROM system_table WHERE name='notice'")
+        text = None
+
+        cursor = self.env.get_read_db().cursor()
+        cursor.execute("SELECT value FROM system WHERE name='notice'")
+        result = cursor.fetchone()
+        if result:
+            (text,) = result
+
         return text
 
     def set_notice(self, text):
         """Insert the notice into the DB"""
-        @self.env.with_transaction()
-        def set_notice_transaction(db, text):
-            cursor = db.cursor()
-            cursor.execute("DELETE FROM system_table WHERE name='notice'")
-            cursor.execute("INSERT INTO system_table (name, value) VALUES ('notice', '%s')" % text)
+        text = text.strip()
 
-        return set_notice_transaction(text)
+        @self.env.with_transaction()
+        def set_notice_transaction(db):
+            cursor = db.cursor()
+            cursor.execute("DELETE FROM system WHERE name='notice'")
+            if text:
+                cursor.execute("INSERT INTO system (name, value) VALUES ('notice', '%s')" % text)
 
     # IAdminPageProvider methods
     def get_admin_panels(self, req):
         if req.perm.has_permission('TRAC_ADMIN'):
-            yield ('general', _('General'))
+            yield ('general', _('General'), 'notice', _('Notice'))
 
     def render_admin_panel(self, req, cat, page, path_info):
         data = {}
 
-        if req.method == 'post':
-            self.set_notice(req.data.get('notice', ''))
+        if req.method == 'POST':
+            self.set_notice(req.args.get('notice', ''))
+            #add_notice(req, _("Saved changes"))
+            req.redirect(req.href.admin(cat, page))
 
         data['notice'] = self.get_notice()
         return 'project_notice_admin.html', data
 
     # ITemplateStreamFilter methods
-    def filter_stream(req, method, filename, stream, data):
+    def filter_stream(self, req, method, filename, stream, data):
         notice = self.get_notice()
+
         if notice:
-            stream |= Transformer("//div[id='mainnav']").after(tag.div(notice, id="project-notice"))
+            stream |= Transformer("//div[@id='mainnav']").after(tag.h1("Notice: " + notice, id="project-notice"))
+
+        return stream
+
+    # ITemplateProvider methods
+    def get_htdocs_dirs(self):
+        """Return the absolute path of a directory containing additional
+        static resources (such as images, style sheets, etc).
+        """
+        from pkg_resources import resource_filename
+        return [('projectnotice', resource_filename(__name__, 'htdocs'))]
+
+    def get_templates_dirs(self):
+        """Return the absolute path of the directory containing the provided
+        ClearSilver/Genshi templates.
+        """
+        from pkg_resources import resource_filename
+        return [resource_filename(__name__, 'templates')]
