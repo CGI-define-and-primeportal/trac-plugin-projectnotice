d53ff7cd53265ec7bf98a7db35f10ac208a1ba93
Danny Milsom <danny.milsom@cgi.com>
2013-05-14 21:23:01 +0000

    Creating another branch for my external users requirement #3129 #3169
    git-svn-id: file:///mnt/ramdisk/d4-svn/branches/features/creatingexternalaccounts@10197 484df14f-75d2-4b3f-8ba0-490e05069a01

---
+++ projectnotice/commands.py
@@ -0,0 +1,25 @@
+from trac.core import Component, implements, TracError
+from trac.perm import IPermissionRequestor
+from tracrpc.api import IXMLRPCHandler
+from projectnotice.web_ui import ProjectNotice
+
+class Commands(Component):
+    implements(IXMLRPCHandler)
+
+    def get_notice(self, req):
+        """Get the current notice or empty string"""
+        pn = ProjectNotice(self.env)
+        return pn.get_notice() or ""
+
+    def set_notice(self, req, text):
+        """Set a new notice, or delete the current one with empty string"""
+        pn = ProjectNotice(self.env)
+        return pn.set_notice(text)
+
+    # IXMLRPCHandler methods
+    def xmlrpc_namespace(self):
+        return 'notice'
+
+    def xmlrpc_methods(self):
+        yield ('PROJECT_LOGIN', ((str,),), self.get_notice)
+        yield ('PROJECT_ADMIN', ((str, str),), self.set_notice)
---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+version='0.1'
+
+setup(name='ProjectNoticePlugin',
+      version=version,
+      description="Apply a notice to a project",
+      long_description="This plugin allows you to apply a textual notice to a project.",
+      author='Adam Piper',
+      author_email='adam.piper@logica.com',
+      url='',
+      keywords='trac plugin project notice',
+      license="BSD-3",
+      install_requires = [ 'Trac>=0.11'],
+      packages=find_packages(exclude=['ez_setup', 'examples', '*tests*']),
+      include_package_data=True,
+      package_data={ 'projectnotice': [
+          'templates/*.html',
+          'htdocs/css/*.css',
+          ] },
+      zip_safe=True,
+      entry_points={'trac.plugins': [
+            'projectnotice.api = projectnotice.api',
+            'projectnotice.web_ui = projectnotice.web_ui',
+            'projectnotice.commands = projectnotice.commands'
+            ]},
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Framework :: Trac',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: 3 Clause BSD (BSD-3)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Bug Tracking',
+          'Topic :: System :: Project Notice',
+          'Topic :: System :: Systems Administration',
+          ],
+      )
